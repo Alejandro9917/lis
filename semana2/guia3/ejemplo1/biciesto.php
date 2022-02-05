c67729d3ea5196e3a0a46596dc01c617e06b7392
Alejandro Melendez <alejandromelendez99@outlook.es>
2022-02-04 23:01:44 -0600

    Guia 3

---
+++ semana2/guia3/ejemplo1/biciesto.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Año bisiesto</title>
+    
+    <link rel="stylesheet" href="css/fonts.css"/>
+    <link rel="stylesheet" href="css/formstyles.css"/>
+    <link rel="stylesheet" media="screen" href="css/biciesto.css">
+    <script src="js/validatedata.js"></script>
+    <script src="js/prefixfree.min.js"></script>
+</head>
+<body>
+    <?php 
+        if(!isset($_POST['enviar'])):
+    ?>
+
+        <form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST" name="frmdatos">
+            <h2 class="contact">Años bisiestos</h2>
+            <span class="contact">
+                <label for="txtdato">Probar año:</label>
+            </span>
+            <input type="text" name="year" id="year" value="" maxlength="20" placeholder="(Ingrese el dato)" /> <br/>
+
+            <span id="numbersOnly">Ingrese números enteros</span>
+            <input type="submit" name="enviar" id="enviar" value="probar" />
+        </form>
+
+    <?php 
+        else:
+            $year = isset($_POST['year']) ? $_POST['year'] : 0;
+            
+            if(($year%4 == 0 && $year%100 != 0) || $year%400 == 0):
+                echo "<p class='bisiesto'>";
+
+            else:
+
+            endif;
+        endif;
+    ?>
+</body>
+</html>
