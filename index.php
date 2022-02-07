ec8a1a1635f32cff42ece5cc6311fb9c6fd47a29
Alejandro Melendez <alejandromelendez99@outlook.es>
2022-02-06 22:02:36 -0600

    Mejorando index

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   </head>
   <body>
 
-    <div class="container p-3">
+    <div class="container t-3">
       <div class="row justify-content-md-center">
 
         <div class="card" style="width: 18rem;">
@@ -33,12 +33,12 @@
       </div>
     </div>
 
-    <div class="modal fade" id="modal-week-1" tabindex="-1" aria-labelledby="modal-week-1" aria-hidden="true">
+    <div class="modal fade" id="modal-week-1" tabindex="-1" aria-labelledby="modal-week-1-Label" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title">Semana 1</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal-week-1" aria-label="Close"></button>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
             <div class="list-group">
@@ -47,18 +47,18 @@
             </div>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal-week-1">Close</button>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="modal fade" id="modal-week-2" tabindex="-1" aria-labelledby="modal-week-2" aria-hidden="true">
+    <div class="modal fade" id="modal-week-2" tabindex="-1" aria-labelledby="modal-week-2-Label" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title">Semana 2</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal-week-2" aria-label="Close"></button>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
             <p>Práctica 2</p>
@@ -67,7 +67,7 @@
               <a href="semana2/practica2/factorial.php" class="list-group-item list-group-item-action">Factorial</a>
               <a href="semana2/practica2/index.php" class="list-group-item list-group-item-action">Index</a>
             </div>
-            <p class="p-3">Guía 3</p>
+            <p class="t-3">Guía 3</p>
             <div class="list-group">
               <a href="semana2/guia3/ejemplo1/biciesto.php" class="list-group-item list-group-item-action">Año biciesto</a>
               <a href="semana2/guia3/ejemplo2/salario.php" class="list-group-item list-group-item-action">Salario</a>
@@ -75,7 +75,7 @@
               <a href="semana2/guia3/ejemplo4/fechahora.php" class="list-group-item list-group-item-action">Fecha Hora</a>
               <a href="semana2/guia3/ejemplo5/compararnum.php" class="list-group-item list-group-item-action">Comparar Números</a>
             </div>
-            <p class="p-3">Guía 4</p>
+            <p class="t-3">Guía 4</p>
             <div class="list-group">
               <a href="semana2/guia4/ejemplo1/convertir.php" class="list-group-item list-group-item-action">Convertir</a>
               <a href="semana2/guia4/ejemplo2/convmonedas.php" class="list-group-item list-group-item-action">Convertir monedas</a>
@@ -84,7 +84,7 @@
             </div>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal-week-2">Close</button>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
           </div>
         </div>
       </div>
